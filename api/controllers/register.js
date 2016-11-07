fa02f5915fe113f095595e60cc3569d942eaedd9
Adam Powers <apowers@ato.ms>
2016-11-06 22:53:53 -0800

    updating base path

--- api/controllers/register.js
+++ api/controllers/register.js
@@ -1,15 +1,16 @@
 "use strict";
 
 module.exports = {
-  register: register
+    register: register
 };
 
 // restify middleware
 function register(req, res) {
-  console.log(req.swagger.params);
-  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  // var name = req.swagger.params.name.value;
+    console.log(req.swagger.params);
+    console.log("!!! REGISTER");
+    // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+    // var name = req.swagger.params.name.value;
 
-  // this sends back a JSON response which is a single string
-  res.json("Testing:");
+    // this sends back a JSON response which is a single string
+    res.json("foobar");
 }
